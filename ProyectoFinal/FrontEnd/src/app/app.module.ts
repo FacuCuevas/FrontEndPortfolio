8a37cd57e1c94acc31c5ed021290ce263c48809e
FacuCuevas <facucuevasar@gmail.com>
2022-08-19 23:06:33 -0300

    creacion-home-login

--- ProyectoFinal/FrontEnd/src/app/app.module.ts
+++ ProyectoFinal/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { SyhComponent } from './components/syh/syh.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -28,12 +31,15 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     SyhComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
     
   ],
   providers: [],
